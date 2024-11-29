fe32e7fa040a0af0b2f4ae6e4d02db47de9f6800
sanBastia <irsanoncode@gmail.com>
2024-11-29 15:26:32 +0700

    chore: 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
   return (
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tight">
-        404 - Page not found
+        404 - Tidak Ditemukan !
       </h1>
       <p className="mb-4">
         Oops! The page you're looking for doesn't seem to exist.
